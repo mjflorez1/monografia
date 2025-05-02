ac45fe08f55aa0157ee48f90b92f33093fe676ff
Gustavo <gustavoquintero9105@gmail.com>
2025-05-02 14:43:00 -0500

    Updated on 05-02-2025 at 14:43

--- codigos/ovo.py
+++ codigos/ovo.py
@@ -29,48 +29,101 @@ def ovo_algorithm(t,y):
     epsilon = 1e-3
     delta = 1e-3
     max_iter = 100
-    max_iter_armijo = 100
+    max_iter_armijo = 1000
     n = 5
     q = 40
 
     xk = np.array([-1,-2,1,-1, 0])
     faux = np.zeros(m)
     Idelta = np.zeros(m,dtype=int)
-    grad = np.zeros(n-1)
+    grad = np.zeros((m,n-1))
     A = np.zeros((m,n))
     b = np.zeros(m)
     c = np.zeros(n)
     c[-1] = 1
     deltax = 1.0
+    theta = 0.5
+
+    while True:    
     
-    x0_bounds = [max(-10 - xk[0], -deltax), min(10 - xk[0], deltax)]
-    x1_bounds = [max(-10 - xk[1], -deltax), min(10 - xk[1], deltax)]
-    x2_bounds = [max(-10 - xk[2], -deltax), min(10 - xk[2], deltax)]
-    x3_bounds = [max(-10 - xk[3], -deltax), min(10 - xk[3], deltax)]
-    x4_bounds = [None, 0]
+        x0_bounds = [max(-10 - xk[0], -deltax), min(10 - xk[0], deltax)]
+        x1_bounds = [max(-10 - xk[1], -deltax), min(10 - xk[1], deltax)]
+        x2_bounds = [max(-10 - xk[2], -deltax), min(10 - xk[2], deltax)]
+        x3_bounds = [max(-10 - xk[3], -deltax), min(10 - xk[3], deltax)]
+        x4_bounds = [None, 0]
 
-    for i in range(m):
-        faux[i] = f_i(xk,t[i],y[i])
+        for i in range(m):
+            faux[i] = f_i(xk,t[i],y[i])
 
-    indices = np.argsort(faux)
-    faux = np.sort(faux)
-    fovo = faux[q]
+        indices = np.argsort(faux)
+        faux = np.sort(faux)
+        fxk = faux[q]
 
-    nconst = mount_Idelta(fovo,faux,indices,delta,Idelta)
+        nconst = mount_Idelta(fxk,faux,indices,delta,Idelta)
 
-    ind = indices[0]
+        for i in range(nconst):
+            ind = indices[i]
+            grad_f_i(xk,t[ind],y[ind],grad[i,:])
 
-    grad_f_i(xk,t[ind],y[ind],grad)
+            A[i,0:n-1] = grad[i,:]
+            A[i,n-1] = -1
 
-    A[0,0:n-1] = grad
-    A[0,n-1] = -1
+        res = linprog(c, A_ub=A[0:nconst,:],b_ub=b[0:nconst],bounds=[x0_bounds,x1_bounds,x2_bounds,x3_bounds,x4_bounds])
 
-    # print(np.shape(A[0,:]),np.shape(c),np.shape(b[0:nconst]))
+        dk = res.x
+        stop_criteria = 0
 
-    res = linprog(c, A_ub=A[0:1,:],b_ub=b[0:1],bounds=[x0_bounds,x1_bounds,x2_bounds,x3_bounds,x4_bounds])
+        # --> Criterio de parada <--
 
-    # print(res.x)
-    print(res.success)
+        for i in range(nconst):
+            mkd = np.dot(grad[i,:],dk[0:n-1])
+
+            if mkd >= stop_criteria: 
+                stop_criteria = mkd
+
+        if abs(mkd) < epsilon:
+            break
+
+        # --> Condición de descenso (Armijo) <--
+
+        alpha = 1
+        xktrial = xk[0:n-1] + alpha * dk[0:n-1]
+
+        for i in range(m):
+            faux[i] = f_i(xktrial,t[i],y[i])
+
+        indices = np.argsort(faux)
+        faux = np.sort(faux)
+        fxktrial = faux[q]
+
+        iter_armijo = 0
+
+        while fxktrial >= fxk + theta * alpha * mkd and iter_armijo <= max_iter_armijo:
+            alpha = 0.5 * alpha
+
+            xktrial = xk[0:n-1] + alpha * dk[0:n-1]
+
+            for i in range(m):
+                faux[i] = f_i(xktrial,t[i],y[i])
+
+            indices = np.argsort(faux)
+            faux = np.sort(faux)
+            fxktrial = faux[q]
+            iter_armijo += 1
+
+        xk = xk[0:n-1] + alpha * dk[0:n-1]
+        xk[-1] = 0
+
+        print(fxk,fxktrial,iter_armijo)
+
+
+        break
+
+
+
+
+        
+
 
 data = np.loadtxt("data.txt")
 
