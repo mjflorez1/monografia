4c9f3a4154753402006ecc6fbab52d56aeeb0508
Mateo <mathewflowers395@gmail.com>
2025-10-23 09:24:25 -0500

    Updated on 10-23-2025 at 09:24

---
+++ codigos/outliers/plot_gauss_cn_outs.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def model(t, x0, x1, x2):
+    return x0 * np.exp((-x1 * ((t - x2)**2)) / 2)
+
+size_img = 0.6
+plt.rcParams.update({'font.size': 11})
+plt.rcParams['figure.figsize'] = [size_img * 6.4,size_img * 4.8]
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+sol_0 = np.loadtxt("txt/gauss_cn/outs_gauss_cn0.txt")
+sol_1 = np.loadtxt("txt/gauss_cn/outs_gauss_cn1.txt")
+sol_2 = np.loadtxt("txt/gauss_cn/outs_gauss_cn2.txt")
+sol_3 = np.loadtxt("txt/gauss_cn/outs_gauss_cn3.txt")
+data = np.loadtxt("txt/data_gauss.txt")
+
+t = np.linspace(data[:,0][0],data[:,0][-1],1000)
+plt.plot(t,model(t,*sol_0),lw=1,label="o = 0")
+plt.plot(t,model(t,*sol_1),lw=1,label="o = 1")
+plt.plot(t,model(t,*sol_2),lw=1,label="o = 2")
+plt.plot(t,model(t,*sol_3),lw=1,label="o = 3")
+plt.plot(data[:,0],data[:,1],"ok",ms=3)
+plt.savefig("figuras/gauss_cn_outs.pdf",bbox_inches="tight")
+plt.legend(fontsize=6,loc="upper left")
+plt.show()
+
---
+++ codigos/outliers/plot_osborne_cn_outs.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def model(t, x0, x1, x2, x3, x4):
+    return x0 + (x1 * np.exp(-t * x3)) + (x2 * np.exp(-t * x4))
+
+size_img = 0.6
+plt.rcParams.update({'font.size': 11})
+plt.rcParams['figure.figsize'] = [size_img * 6.4,size_img * 4.8]
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+sol_0 = np.loadtxt("txt/osborne_cn/outs_osborne_cn0.txt")
+sol_1 = np.loadtxt("txt/osborne_cn/outs_osborne_cn1.txt")
+sol_2 = np.loadtxt("txt/osborne_cn/outs_osborne_cn2.txt")
+sol_3 = np.loadtxt("txt/osborne_cn/outs_osborne_cn3.txt")
+sol_4 = np.loadtxt("txt/osborne_cn/outs_osborne_cn4.txt")
+sol_5 = np.loadtxt("txt/osborne_cn/outs_osborne_cn5.txt")
+data = np.loadtxt("txt/data_osborne1.txt")
+
+t = np.linspace(data[:,0][0],data[:,0][-1],1000)
+plt.plot(t,model(t,*sol_0),lw=1,label="o = 0")
+plt.plot(t,model(t,*sol_1),lw=1,label="o = 1")
+plt.plot(t,model(t,*sol_2),lw=1,label="o = 2")
+plt.plot(t,model(t,*sol_3),lw=1,label="o = 3")
+plt.plot(t,model(t,*sol_4),lw=1,label="o = 4")
+plt.plot(t,model(t,*sol_5),lw=1,label="o = 5")
+plt.plot(data[:,0],data[:,1],"ok",ms=3)
+plt.savefig("figuras/osborne_cn_outs.pdf",bbox_inches="tight")
+plt.legend(fontsize=6,loc="lower left")
+plt.show()
